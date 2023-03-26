14fc3e16f776e2d8e6dd0f593aaf3379a14f90ed
pabfam <pabalate@Pabalates-MBP.attlocal.net>
2023-03-26 16:47:58 -0700

    styled css for nav bar to have hamburger

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,45 @@
 import "./NavBarStyles.css"
-import React from 'react'
-import {Link} from "react-router-dom"
+import React, { useState } from 'react'
+
+import { Link } from "react-router-dom"
+import { FaBars, FaTimes } from "react-icons/fa"
+
+
 const Navbar = () => {
-  return (
-    <div className="header">
-        <Link to="/">
-            <h1>Portfolio</h1>
-        </Link>
-        <ul className="nav-menu">
-            <li>
-                <Link to="/">Home</Link>
-            </li>
-            <li>
-                <Link to="/project">Project</Link>
-            </li>
-            <li>
-                <Link to="/about">About</Link>
-            </li>
-            <li>
-                <Link to="/contact">Contact</Link>
-            </li>
-        </ul>
-    </div>
-  )
+
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+
+
+    return (
+        <div className="header">
+            <Link to="/">
+                <h1>Portfolio</h1>
+            </Link>
+            <ul className={click ? "nav-menu active" :
+                "nav-menu"}>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/project">Project</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+            </ul>
+            <div className="hamburger" onClick={handleClick}>
+                {click ? (
+                    <FaTimes size={20} style={{ color: "#fff" }} />
+                ) : (
+                    <FaBars size={20} style={{ color: "#fff" }} />
+                )}
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
