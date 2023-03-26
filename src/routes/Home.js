ad0ede924fb0edc9c0b872b8de99a601390dce3a
pabfam <pabalate@Pabalates-MBP.attlocal.net>
2023-03-26 16:58:23 -0700

    added NavBar and Footer

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import HeroImg from '../components/HeroImg'
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
+      <HeroImg/>
     </div>
   )
 }
-
-export default Home
