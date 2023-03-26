d82f334d8e00e104378f8af8cc5ef76feec006bc
pabfam <pabalate@Pabalates-MBP.attlocal.net>
2023-03-26 16:42:17 -0700

    added routes, added components, and put routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import './index.css';
+import Home from "./routes/Home"
+import About from "./routes/About"
+import Project from "./routes/Project"
+import Contact from "./routes/Contact"
+import {Route, Routes } from "react-router-dom"
 
 function App() {
   return (
-    <h2>Portfolio Project</h2>
+    <>
+     <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/project" element={<Project/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/contact" element={<Contact/>} />
+     </Routes>
+    </>
   );
 }
-
-export default App;
