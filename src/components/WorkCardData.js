4539f677added63a3530b5fc747eab08aad790cc
pabfam <pabalate@Pabalates-MBP.attlocal.net>
2023-03-26 17:19:14 -0700

    added work code and formstyles and added assets

---
+++ src/components/WorkCardData.js
@@ -0,0 +1,33 @@
+import pro1 from "../assets/social_media_api.png"
+import pro2 from "../assets/Tech_Blog.png"
+import pro3 from "../assets/text_editor.png"
+import pro4 from "../assets/progrest.png"
+
+const ProjectCardData = [
+    {
+        imgsrc: pro1,
+        title: "Social Network API",
+        text: "Social Network API",
+        view: "https://github.com/rypab4/social-network-api"
+    },
+    {
+        imgsrc: pro2,
+        title: "Tech Blog",
+        text: "Tech Blog",
+        view: "https://thetechnologyblog031023.herokuapp.com/"
+    },
+    {
+        imgsrc: pro3,
+        title: "text editor",
+        text: "Tech editor",
+        view: "https://text-editor-2023-again.herokuapp.com/"
+    },
+    {
+        imgsrc: pro4,
+        title: "Progrest",
+        text: "Progrest",
+        view: "https://progrest.herokuapp.com/"
+    },
+]
+
+export default ProjectCardData
