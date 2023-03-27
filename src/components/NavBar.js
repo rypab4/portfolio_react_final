313fa6f46ed47a821b37826d3f717a4df02c2338
pabfam <pabalate@Pabalates-MBP.attlocal.net>
2023-03-26 17:27:02 -0700

    fixed the navbar and styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import "./NavbarStyles.css"
+import "./NavBarStyles.css"
 import React, { useState } from 'react'
 
 import { Link } from "react-router-dom"
--- src/routes/About.js
+++ src/routes/About.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import HeroImg2 from '../components/HeroImg2'
+import AboutContent from '../components/AboutContent'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>  
+    <Navbar/>
+    <HeroImg2 heading="ABOUT." text="I am a cool Front-End Developer"/>
+    <AboutContent/>
+    <Footer/>
+  </div>
   )
 }
 
